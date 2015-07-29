d7267df579539144b5bff1195a0b803eff13a1be
daryllabar <d3labar@hotmail.com>
2015-07-29 16:34:50 -0400

    Moved GetAttributeNames and GetLogical AttributeName

--- DLaB.Xrm/Extensions.cs
+++ DLaB.Xrm/Extensions.cs
@@ -66,45 +66,6 @@ namespace DLaB.Xrm
             return columnSet;
         }
 
-        /// <summary>
-        /// Creates an array of attribute names array from an Anonymous Type Initializer.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="anonymousTypeInitializer">The anonymous type initializer.</param>
-        /// <returns></returns>
-        /// <exception cref="System.ArgumentException">lambda must return an object initializer</exception>
-        private static string[] GetAttributeNamesArray<T>(Expression<Func<T, object>> anonymousTypeInitializer) where T : Entity
-        {
-            var initializer = anonymousTypeInitializer.Body as NewExpression;
-            if (initializer == null || initializer.Members == null)
-                throw new ArgumentException("lambda must return an object initializer");
-
-            // Search for and replace any occurence of Id with the actual Entity's Id
-            return initializer.Members.Select(GetLogicalAttributeName<T>).ToArray();
-        }
-
-        /// <summary>
-        /// Normally just returns the name of the property, in lowercase.  But Id must be looked up via reflection.
-        /// </summary>
-        /// <param name="property"></param>
-        /// <returns></returns>
-        private static string GetLogicalAttributeName<T>(MemberInfo property) where T : Entity
-        {
-            var name = property.Name.ToLower();
-            if (name == "id")
-            {
-                var attribute = typeof(T).GetProperty("Id").GetCustomAttributes<AttributeLogicalNameAttribute>().FirstOrDefault();
-                if (attribute == null)
-                {
-                    throw new ArgumentException(property.Name + " does not contain an AttributeLogicalNameAttribute.  Unable to determine id");
-                }
-
-                name = attribute.LogicalName;
-            }
-
-            return name;
-        }
-
         #endregion // ColumnSet
 
         #region Entity
@@ -580,6 +541,45 @@ namespace DLaB.Xrm
             var reference = entity.ToEntityReference();
             reference.Name = name;
             return reference;
+        }
+
+        /// <summary>
+        /// Creates an array of attribute names array from an Anonymous Type Initializer.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="anonymousTypeInitializer">The anonymous type initializer.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentException">lambda must return an object initializer</exception>
+        private static string[] GetAttributeNamesArray<T>(Expression<Func<T, object>> anonymousTypeInitializer) where T : Entity
+        {
+            var initializer = anonymousTypeInitializer.Body as NewExpression;
+            if (initializer == null || initializer.Members == null)
+                throw new ArgumentException("lambda must return an object initializer");
+
+            // Search for and replace any occurence of Id with the actual Entity's Id
+            return initializer.Members.Select(GetLogicalAttributeName<T>).ToArray();
+        }
+
+        /// <summary>
+        /// Normally just returns the name of the property, in lowercase.  But Id must be looked up via reflection.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        private static string GetLogicalAttributeName<T>(MemberInfo property) where T : Entity
+        {
+            var name = property.Name.ToLower();
+            if (name == "id")
+            {
+                var attribute = typeof(T).GetProperty("Id").GetCustomAttributes<AttributeLogicalNameAttribute>().FirstOrDefault();
+                if (attribute == null)
+                {
+                    throw new ArgumentException(property.Name + " does not contain an AttributeLogicalNameAttribute.  Unable to determine id");
+                }
+
+                name = attribute.LogicalName;
+            }
+
+            return name;
         }
 
         /// <summary>
