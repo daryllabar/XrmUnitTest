d92c2bdda117738fd215d5264324215926ac8b88
Daryl LaBar <d3labar@hotmail.com>
2016-10-17 00:08:02 -0400

    Removed Fakes Dependency

--- Example.MsTest/MsFakesVsXrmUnitTestExampleTests.cs
+++ Example.MsTest/MsFakesVsXrmUnitTestExampleTests.cs
@@ -15,60 +15,64 @@ namespace Example.MsTest
     [TestClass]
     public class MsFakesVsXrmUnitTestExampleTests
     {
+        // MsFakes Isn't suppored by Appveyor.  To test:
+        // 1. Right click on the Microsoft.Xrm.Sdk.dll in the references, and select "Add Fakes Assembly"
+        // 2. May need to overwrite Fakes/Microsoft.Xrm.Sdk.fakes file.
+        // 3. Uncomment this test.
         /// <summary>
         /// Example test for running a unit test using MS Fakes
         /// </summary>
-        [TestMethod]
-        public void MakeNameMatchCase_NameIsMcdonald_Should_UpdateToMcDonald_MsFakes()
-        {
-            //
-            // Arrange
-            //
-            Entity updatedEntity = null;
-            IOrganizationService service = new Microsoft.Xrm.Sdk.Fakes.StubIOrganizationService()
-            {
-                ExecuteOrganizationRequest = request =>
-                {
-                    if (request is RetrieveMultipleRequest)
-                    {
-                        var response = new RetrieveMultipleResponse
-                        {
-                            Results =
-                            {
-                                ["EntityCollection"] = new EntityCollection()
-                            }
-                        };
-            
-                        response.EntityCollection.Entities.Add(
-                            new Contact
-                            {
-                                Id = Guid.NewGuid(),
-                                LastName = "Mcdonald"
-                            });
-                        return response;
-                    }
-            
-                    var updateRequest = request as UpdateRequest;
-                    if (updateRequest == null)
-                    {
-                        throw new NotImplementedException("Unrecognized Request");
-                    }
-                    updatedEntity = updateRequest.Target;
-                    return new UpdateResponse();
-                },
-            };
-            
-            // 
-            // Act
-            // 
-            RenameLogic.MakeNameMatchCase(service, "McDonald");
-            
-            //
-            // Assert
-            // 
-            Assert.IsNotNull(updatedEntity);
-            Assert.AreEqual("McDonald", updatedEntity.ToEntity<Contact>().LastName);
-        }
+        //[TestMethod]
+        //public void MakeNameMatchCase_NameIsMcdonald_Should_UpdateToMcDonald_MsFakes()
+        //{
+        //    //
+        //    // Arrange
+        //    //
+        //    Entity updatedEntity = null;
+        //    IOrganizationService service = new Microsoft.Xrm.Sdk.Fakes.StubIOrganizationService()
+        //    {
+        //        ExecuteOrganizationRequest = request =>
+        //        {
+        //            if (request is RetrieveMultipleRequest)
+        //            {
+        //                var response = new RetrieveMultipleResponse
+        //                {
+        //                    Results =
+        //                    {
+        //                        ["EntityCollection"] = new EntityCollection()
+        //                    }
+        //                };
+        //    
+        //                response.EntityCollection.Entities.Add(
+        //                    new Contact
+        //                    {
+        //                        Id = Guid.NewGuid(),
+        //                        LastName = "Mcdonald"
+        //                    });
+        //                return response;
+        //            }
+        //    
+        //            var updateRequest = request as UpdateRequest;
+        //            if (updateRequest == null)
+        //            {
+        //                throw new NotImplementedException("Unrecognized Request");
+        //            }
+        //            updatedEntity = updateRequest.Target;
+        //            return new UpdateResponse();
+        //        },
+        //    };
+        //    
+        //    // 
+        //    // Act
+        //    // 
+        //    RenameLogic.MakeNameMatchCase(service, "McDonald");
+        //    
+        //    //
+        //    // Assert
+        //    // 
+        //    Assert.IsNotNull(updatedEntity);
+        //    Assert.AreEqual("McDonald", updatedEntity.ToEntity<Contact>().LastName);
+        //}
 
         /// <summary>
         /// Example test for running a unit test only in memory, as compared to the MS Fakes
