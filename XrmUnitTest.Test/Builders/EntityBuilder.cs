f5971ab42a7c129d55f56d7959eed776a0b0dd9e
Daryl LaBar <d3labar@hotmail.com>
2018-05-24 13:09:43 -0400

    Remove old Code

--- XrmUnitTest.Test/Builders/EntityBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xrm.Sdk;
-
-namespace XrmUnitTest.Test.Builders
-{
-    public abstract class EntityBuilder<TEntity> : DLaB.Xrm.Test.Builders.EntityBuilder<TEntity> where TEntity : Entity
-    {
-
-    }
-}
