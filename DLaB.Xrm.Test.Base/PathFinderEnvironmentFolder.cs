c3f6c8c75ecfea64c26eb7e7f78e9cb9fc5d43be
Daryl LaBar <d3labar@hotmail.com>
2022-09-07 10:26:20 -0400

    Added missing comments

--- DLaB.Xrm.Test.Base/PathFinderEnvironmentFolder.cs
+++ DLaB.Xrm.Test.Base/PathFinderEnvironmentFolder.cs
@@ -7,21 +7,36 @@ using DLaB.Xrm.Test;
 
 namespace DataverseUnitTest
 {
+    /// <summary>
+    /// Finds a relative path using an environment folder as the root path.
+    /// </summary>
     public class PathFinderEnvironmentFolder: IPathFinder
     {
         private readonly Environment.SpecialFolder _folder;
         private readonly string _relativePath;
 
+        /// <summary>
+        /// Defaults to using the Environment.SpecialFolder.ApplicationData Folder.
+        /// </summary>
+        /// <param name="relativePath">The path.</param>
         public PathFinderEnvironmentFolder(string relativePath): this(Environment.SpecialFolder.ApplicationData, relativePath)
         {
         }
 
+        /// <summary>
+        /// Uses the given special Folder and relative path.
+        /// </summary>
+        /// <param name="folder">The Special Folder.</param>
+        /// <param name="relativePath">The path.</param>
         public PathFinderEnvironmentFolder(Environment.SpecialFolder folder, string relativePath)
         {
             _folder = folder;
             _relativePath = relativePath;
         }
 
+        /// <summary>
+        /// Returns the path.
+        /// </summary>
         public string GetPath()
         {
             return string.IsNullOrWhiteSpace(_relativePath)
